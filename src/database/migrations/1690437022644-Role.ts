fb185c6b1968a1dca0105c6981f28799c8a7a97a
farrukh zeb <farrukh.zeb@emumba.com>
2023-07-27 12:03:04 +0500

    feat: added migration files

---
+++ src/database/migrations/1690437022644-Role.ts
@@ -0,0 +1,28 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class Role1690437022644 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'role',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'name',
+            type: 'varchar(255)',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('role');
+  }
+}
---
+++ src/database/migrations/1690437234031-CreateUser.ts
@@ -0,0 +1,83 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export class CreateUser1690437234031 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'user',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'email',
+            type: 'varchar(255)',
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar(255)',
+          },
+          {
+            name: 'firstName',
+            type: 'varchar(255)',
+          },
+          {
+            name: 'lastName',
+            type: 'varchar(255)',
+            isNullable: true,
+          },
+          {
+            name: 'profileImage',
+            type: 'varchar(255)',
+            isNullable: true,
+          },
+          {
+            name: 'createdAt',
+            type: 'date',
+            default: 'now()',
+          },
+          {
+            name: 'updatedAt',
+            type: 'date',
+            default: 'now()',
+          },
+          {
+            name: 'roleId',
+            type: 'int',
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'user',
+      new TableForeignKey({
+        columnNames: ['roleId'],
+        referencedTableName: 'role',
+        referencedColumnNames: ['id'],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    // Drop foreign key constraint from "user" table
+    const table = await queryRunner.getTable('user');
+    const foreignKey = table.foreignKeys.find(
+      (fk) => fk.columnNames.indexOf('roleId') !== -1,
+    );
+    if (foreignKey) {
+      await queryRunner.dropForeignKey('user', foreignKey);
+    }
+    await queryRunner.dropTable('user');
+  }
+}
