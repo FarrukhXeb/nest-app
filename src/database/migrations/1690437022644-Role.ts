c04a0d6c198e913bc90b5fc7120ff519b6750fe2
farrukh zeb <farrukh.zeb@emumba.com>
2023-08-04 16:00:27 +0500

    chore: updated the migrations

--- src/database/migrations/1690437022644-Role.ts
+++ src/database/migrations/1690437022644-Role.ts
@@ -2,24 +2,13 @@ import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class Role1690437022644 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.createTable(
-      new Table({
-        name: 'role',
-        columns: [
-          {
-            name: 'id',
-            type: 'int',
-            isPrimary: true,
-            isGenerated: true,
-            generationStrategy: 'identity',
-          },
-          {
-            name: 'name',
-            type: 'varchar(255)',
-          },
-        ],
-      }),
-    );
+    await queryRunner.query(`
+      CREATE TABLE "role" (
+        "id" SERIAL NOT NULL,
+        "name" character varying NOT NULL,
+        CONSTRAINT "PK_b36bcfe02fc8de3c57a8b2391c2" PRIMARY KEY ("id")
+      );
+    `);
     await queryRunner.query(
       `INSERT INTO role (name) VALUES  ('admin'), ('user')`,
     );
---
+++ src/database/migrations/1691145850764-CreateUserPoll.ts
@@ -0,0 +1,27 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateUserPoll1691145850764 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+        CREATE TABLE "user_polls_poll" (
+            "userId" integer NOT NULL,
+            "pollId" integer NOT NULL,
+            CONSTRAINT "FK_9f8fd2dd2d001511526c88860d1" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE,
+            CONSTRAINT "FK_119e9b5e1a0f61c3800eeebc196" FOREIGN KEY ("pollId") REFERENCES "poll"("id") ON DELETE NO ACTION ON UPDATE NO ACTION,
+            CONSTRAINT "PK_47f35a7f4f66631ec4bd95ceabd" PRIMARY KEY ("userId", "pollId")
+        );
+
+        CREATE INDEX "IDX_9f8fd2dd2d001511526c88860d" ON "user_polls_poll" ("userId");
+        CREATE INDEX "IDX_119e9b5e1a0f61c3800eeebc19" ON "user_polls_poll" ("pollId");
+
+        INSERT INTO "user_polls_poll" ("userId", "pollId") VALUES (1, 1);
+        INSERT INTO "user_polls_poll" ("userId", "pollId") VALUES (2, 1);
+        INSERT INTO "user_polls_poll" ("userId", "pollId") VALUES (3, 1);
+        INSERT INTO "user_polls_poll" ("userId", "pollId") VALUES (4, 1);
+    `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('user_polls_poll');
+  }
+}
